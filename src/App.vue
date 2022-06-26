ff0910eb3cfff7dd1052436078ab4c2c539f849e
Amazia Lugassy <lugas69@gmail.com>
2022-06-26 19:15:04 +0300

    header and resources components added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-  <div></div>
+  <the-header title="RemeberMe"></the-header>
 </template>
 
 <script>
 
+import TheHeader from './components/UI/TheHeader.vue';
+
 export default {
+  components: {
+    TheHeader
+  },
   name: 'App',
-  // components: {
-    
-  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
 </style>
