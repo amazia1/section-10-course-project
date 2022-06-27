713118335a74f00fbcdd447c71529679dda93cfc
Amazia Lugassy <lugas69@gmail.com>
2022-06-27 14:31:28 +0300

    add-resource and stored-resources components added

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,17 +1,41 @@
 
 <template>
     <base-card>
-        <base-button @click="setSelectedTab('resources')" type="button" :mode="isResourcesTabSelected">Resources</base-button>
+        <base-button @click="setSelectedTab('stored-resources')" type="button" :mode="isResourcesTabSelected">Resources</base-button>
         <base-button @click="setSelectedTab('add-resource')" type="button" :mode="isAddResourceTabSelected">Add Resource</base-button>
+    </base-card>
+
+    <base-card>
+        <component :is="selectedTab"></component>
     </base-card>
 </template>
 
 <script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
 
 export default {
+    components: {
+        StoredResources,
+        AddResource
+    },
     data() {
         return {
-            selectedTab: 'resources'
+            selectedTab: 'stored-resources',
+            resources: [
+                {
+                    id: 'official-guide',
+                    title: 'Ofiicial Guide',
+                    description: 'The Official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn how to google',
+                    link: 'https://google.com'
+                },
+            ]
         }
     },
     methods: {
@@ -21,7 +45,7 @@ export default {
     },
     computed: {
         isResourcesTabSelected() {
-            return this.selectedTab === 'resources' ? null : 'flat';
+            return this.selectedTab === 'stored-resources' ? null : 'flat';
         },
         isAddResourceTabSelected() {
             return this.selectedTab === 'add-resource' ? null : 'flat';
