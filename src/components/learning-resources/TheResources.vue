b346fc397a14262a9f32c906f388de8e9c00a6ef
Amazia Lugassy <lugas69@gmail.com>
2022-06-27 12:36:35 +0300

    additions

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,11 +1,29 @@
 
 <template>
-    
+    <base-button @click="setSelectedTab('resources')" type="button" :mode="isResourcesTabSelected">Resources</base-button>
+    <base-button @click="setSelectedTab('add-resource')" type="button" :mode="isAddResourceTabSelected">Add Resource</base-button>
 </template>
 
 <script>
 
 export default {
-    
+    data() {
+        return {
+            selectedTab: 'resources'
+        }
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    },
+    computed: {
+        isResourcesTabSelected() {
+            return this.selectedTab === 'resources' ? null : 'flat';
+        },
+        isAddResourceTabSelected() {
+            return this.selectedTab === 'add-resource' ? null : 'flat';
+        }
+    }
 }
 </script>
